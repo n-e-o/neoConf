0c9b564077e3e65e812e116182325bc00c802dfc
Neo- <neo-@singualr-dreams.com>
2014-06-27 15:38:20 +0200

    add zsh config

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install necessary packages
-sudo apt-get install git emacs zsh 
+sudo apt-get install git emacs zsh ssh
 
 # Get oh-my-zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
@@ -15,4 +15,11 @@ function installFiles()
 	cp -r ${script_dir}/homeDirFiles/. ~/
 }
 
+# Copy all local home files
 installFiles
+
+# Set Zsh as best shell ever
+chsh -s `which zsh`
+
+# Set Cinnamon theme
+gsettings set org.cinnamon.theme name 'Dark_Void'
